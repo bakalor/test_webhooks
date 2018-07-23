cb0a1b156f1b3ed4191e95260646811b8444303f
Igor Bakalo <Igor_Bakalo@epam.com>
2018-07-23 18:30:55 +0300

    commit 2 2 test web hook

--- get_secrets_manager_data.py
+++ get_secrets_manager_data.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 from botocore.exceptions import ClientError
-# comment for test commit 2
+# comment for test commit 3
 def get_secret():
     secret_name = ""
     endpoint_url = "https://secretsmanager.us-east-1.amazonaws.com"
